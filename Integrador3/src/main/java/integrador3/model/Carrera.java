966bf12534b8d6f5c3cb4e4b899c2143f6185f97
Doroteo Vidal <doroteo17vidal@hotmail.com>
2023-09-29 10:27:16 -0300

    se agregan entidades y configuracion para mysql

---
+++ Integrador3/src/main/java/integrador3/model/Carrera.java
@@ -0,0 +1,77 @@
+package integrador3.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+
+@Entity
+public class Carrera {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nombre;
+	
+	@Column
+	private int duracion;
+	
+	@OneToMany(mappedBy = "carrera", fetch = FetchType.LAZY)
+	private List<EstudianteCarrera> estudiantes;
+
+	public Carrera() {}
+
+	public Carrera(String nombre, int duracion) {
+		super();
+		this.nombre = nombre;
+		this.duracion = duracion;
+		this.estudiantes = new ArrayList<>();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getDuracion() {
+		return duracion;
+	}
+
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public List<EstudianteCarrera> getEstudiantes() {
+		return estudiantes;
+	}
+
+	public void setEstudiantes(List<EstudianteCarrera> estudiantes) {
+		this.estudiantes = estudiantes;
+	}
+
+	@Override
+	public String toString() {
+		return "Carrera [id=" + id + ", nombre=" + nombre + ", duracion=" + duracion + "]";
+	}
+
+}
---
+++ Integrador3/src/main/java/integrador3/model/EstudianteCarrera.java
@@ -0,0 +1,93 @@
+package integrador3.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class EstudianteCarrera {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(cascade = CascadeType.MERGE)
+	@JoinColumn(name = "fk_estudiante")
+	private Estudiante estudiante;
+	
+	@ManyToOne(cascade = CascadeType.MERGE)
+	@JoinColumn(name = "fk_carrera")
+	private Carrera carrera;
+	
+	@Column(name = "fecha_inscripcion")
+	private Long fechaInscripcion;
+	
+	@Column(name = "fecha_graduacion")
+	private Long fechaGraduacion;
+	
+	@Column
+	private int antiguedad;
+	
+	public EstudianteCarrera() {}
+
+	public EstudianteCarrera(Estudiante estudiante, Carrera carrera, Long fechaInscripcion, Long fechaGraduacion, int antiguedad) {
+		super();
+		this.estudiante = estudiante;
+		this.carrera = carrera;
+		this.fechaInscripcion = fechaInscripcion;
+		this.fechaGraduacion = fechaGraduacion;
+		this.antiguedad = antiguedad;
+	}
+
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
+
+	public Carrera getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+
+	public Long getFechaInscripcion() {
+		return fechaInscripcion;
+	}
+
+	public void setFechaInscripcion(Long fechaInscripcion) {
+		this.fechaInscripcion = fechaInscripcion;
+	}
+
+	public Long getFechaGraduacion() {
+		return fechaGraduacion;
+	}
+
+	public void setFechaGraduacion(Long fechaGraduacion) {
+		this.fechaGraduacion = fechaGraduacion;
+	}
+
+	public int getAntiguedad() {
+		return antiguedad;
+	}
+
+	public void setAntiguedad(int antiguedad) {
+		this.antiguedad = antiguedad;
+	}
+
+	@Override
+	public String toString() {
+		return "EstudianteCarrera [estudiante=" + estudiante + ", carrera=" + carrera + ", fechaInscripcion="
+				+ fechaInscripcion + ", fechaGraduacion=" + fechaGraduacion + ", antiguedad=" + antiguedad + "]";
+	}
+
+}
